59cb2f0bacb00aaff708d3fb8f976a9ac43007ec
George Durieux <giorgosdurieux@gmail.com>
2025-05-26 20:35:44 +0300

    feat: new components

---
+++ src/components/ArrowFunctionalComponentWithPropsType.tsx
@@ -0,0 +1,15 @@
+type Props = {
+    title: string
+    description: string
+}
+
+const ArrowFunctionalComponentWithPropsType = ({title, description}: Props) => {
+    return (
+        <>
+            <h1 className='text-center mt-12 text-xl font-bold'>{title}</h1>
+            <p className='text-gray-800'>{description}</p>
+        </>
+    )
+}
+
+export default ArrowFunctionalComponentWithPropsType
